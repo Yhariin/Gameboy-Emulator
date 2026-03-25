f02b1f5710d3b62bdc859730a94fa4a9e2aa4f50
Cristian Suarez <suarezcristian137@gmail.com>
2026-03-24 22:49:18 -0400

    0xCB instructions

--- src/emulator/cpu.cpp
+++ src/emulator/cpu.cpp
@@ -94,6 +94,10 @@ static void cpu_process()
                 f_register_set_c(bit8 & cpu_state->registers.a);
                 cpu_state->registers.a = cpu_state->registers.a << 1 | f_register_get_c();
 
+                f_register_set_z(false);
+                f_register_set_n(false);
+                f_register_set_h(false);
+
                 cpu_state->pc += 1;
                 break;
             }
@@ -189,6 +193,10 @@ static void cpu_process()
                 f_register_set_c(bit1 & cpu_state->registers.a);
                 cpu_state->registers.a = cpu_state->registers.a >> 1 | (f_register_get_c() << 7);
 
+                f_register_set_z(false);
+                f_register_set_n(false);
+                f_register_set_h(false);
+
                 cpu_state->pc += 1;
                 break;
             }
@@ -261,7 +269,12 @@ static void cpu_process()
             // RLA
             case 0x17:
             {
+                f_register_set_c(bit8 & cpu_state->registers.a);
                 cpu_state->registers.a = cpu_state->registers.a << 1 | f_register_get_c();
+
+                f_register_set_z(false);
+                f_register_set_n(false);
+                f_register_set_h(false);
 
                 cpu_state->pc += 1;
                 break;
@@ -339,7 +352,12 @@ static void cpu_process()
             // RRA
             case 0x1F:
             {
+                f_register_set_c(bit1 & cpu_state->registers.a);
                 cpu_state->registers.a = cpu_state->registers.a >> 1 | (f_register_get_c() << 7);
+
+                f_register_set_z(false);
+                f_register_set_n(false);
+                f_register_set_h(false);
 
                 cpu_state->pc += 1;
                 break;
@@ -1855,5 +1873,603 @@ static void set_HL(u16 value)
 
 static void process_16_bit_opcodes(u8 low)
 {
+    u8 *src;
+    switch ((low & 0x0F) % 8)
+    {
+        case 0x00:
+            src = &cpu_state->registers.b;
+            break;
+        case 0x01:
+            src = &cpu_state->registers.c;
+            break;
+        case 0x02:
+            src = &cpu_state->registers.d;
+            break;
+        case 0x03:
+            src = &cpu_state->registers.e;
+            break;
+        case 0x04:
+            src = &cpu_state->registers.h;
+            break;
+        case 0x05:
+            src = &cpu_state->registers.l;
+            break;
+        case 0x06:
+            src = &cpu_state->memory[get_HL()];
+            break;
+        case 0x07:
+            src = &cpu_state->registers.a;
+            break;
+    }
 
+    switch(low)
+    {
+        // Rotations
+        // RLC
+        case 0x00:
+        case 0x01:
+        case 0x02:
+        case 0x03:
+        case 0x04:
+        case 0x05:
+        case 0x06:
+        case 0x07:
+        //RRC
+        case 0x08:
+        case 0x09:
+        case 0x0A:
+        case 0x0B:
+        case 0x0C:
+        case 0x0D:
+        case 0x0E:
+        case 0x0F:
+        // RL
+        case 0x10:
+        case 0x11:
+        case 0x12:
+        case 0x13:
+        case 0x14:
+        case 0x15:
+        case 0x16:
+        case 0x17:
+        //RR
+        case 0x18:
+        case 0x19:
+        case 0x1A:
+        case 0x1B:
+        case 0x1C:
+        case 0x1D:
+        case 0x1E:
+        case 0x1F:
+        {
+
+            if (low >> 4 == 0x00) // 0x00
+            {
+                if (low & 0x0F < 0x08)
+                {
+                    // RLC src
+                    f_register_set_c(bit8 & *src);
+                    *src = *src << 1 | f_register_get_c();
+
+                    f_register_set_z(*src == 0);
+                    f_register_set_n(false);
+                    f_register_set_h(false);
+                }
+                else
+                {
+                    // RRC
+                    f_register_set_c(bit1 & *src);
+                    *src = *src >> 1 | (f_register_get_c() << 7);
+
+                    f_register_set_z(*src == 0);
+                    f_register_set_n(false);
+                    f_register_set_h(false);
+                }
+            }
+            else // 0x10
+            {
+                if (low & 0x0F < 0x08)
+                {
+                    // RL src
+                    f_register_set_c(bit8 & *src);
+                    *src = *src << 1 | f_register_get_c();
+
+                    f_register_set_z(*src == 0);
+                    f_register_set_n(false);
+                    f_register_set_h(false);
+                }
+                else
+                {
+                    // RR
+                    f_register_set_c(bit1 & *src);
+                    *src = *src >> 1 | (f_register_get_c() << 7);
+
+                    f_register_set_z(*src == 0);
+                    f_register_set_n(false);
+                    f_register_set_h(false);
+                }
+
+            }
+            break;
+        }
+
+        // Shifts
+        // SLA
+        case 0x20:
+        case 0x21:
+        case 0x22:
+        case 0x23:
+        case 0x24:
+        case 0x25:
+        case 0x26:
+        case 0x27:
+        // SRA
+        case 0x28:
+        case 0x29:
+        case 0x2A:
+        case 0x2B:
+        case 0x2C:
+        case 0x2D:
+        case 0x2E:
+        case 0x2F:
+        {
+            if (low & 0x0F < 0x08)
+            {
+                // SLA
+                f_register_set_c(bit8 & *src);
+                *src <<= 1;
+
+                f_register_set_z(*src == 0);
+                f_register_set_n(false);
+                f_register_set_h(false);
+            }
+            else
+            {
+                // SRA
+                u8 tmp = bit8 & *src;
+
+                f_register_set_c(bit1 & *src);
+                *src >>= 1;
+                *src |= tmp;
+
+                f_register_set_z(*src == 0);
+                f_register_set_n(false);
+                f_register_set_h(false);
+            }
+
+            break;
+        }
+
+        // SWAP
+        case 0x30:
+        case 0x31:
+        case 0x32:
+        case 0x33:
+        case 0x34:
+        case 0x35:
+        case 0x36:
+        case 0x37:
+        // SRL
+        case 0x38:
+        case 0x39:
+        case 0x3A:
+        case 0x3B:
+        case 0x3C:
+        case 0x3D:
+        case 0x3E:
+        case 0x3F:
+        {
+            if (low & 0x0F < 0x08)
+            {
+                // SWAP
+                u8 low_4 = *src & 0x07;
+                u8 hi_4 = *src >> 4;
+                *src = (low_4 << 4) | hi_4;
+
+                f_register_set_z(*src == 0);
+                f_register_set_n(false);
+                f_register_set_h(false);
+                f_register_set_c(false);
+            }
+            else
+            {
+                // SRL
+                f_register_set_c(bit1 & *src);
+                *src >>= 1;
+
+                f_register_set_z(*src == 0);
+                f_register_set_n(false);
+                f_register_set_h(false);
+            }
+
+            break;
+        }
+
+        // BIT
+        case 0x40:
+        case 0x41:
+        case 0x42:
+        case 0x43:
+        case 0x44:
+        case 0x45:
+        case 0x46:
+        case 0x47:
+        case 0x48:
+        case 0x49:
+        case 0x4A:
+        case 0x4B:
+        case 0x4C:
+        case 0x4D:
+        case 0x4E:
+        case 0x4F:
+        case 0x50:
+        case 0x51:
+        case 0x52:
+        case 0x53:
+        case 0x54:
+        case 0x55:
+        case 0x56:
+        case 0x57:
+        case 0x58:
+        case 0x59:
+        case 0x5A:
+        case 0x5B:
+        case 0x5C:
+        case 0x5D:
+        case 0x5E:
+        case 0x5F:
+        case 0x60:
+        case 0x61:
+        case 0x62:
+        case 0x63:
+        case 0x64:
+        case 0x65:
+        case 0x66:
+        case 0x67:
+        case 0x68:
+        case 0x69:
+        case 0x6A:
+        case 0x6B:
+        case 0x6C:
+        case 0x6D:
+        case 0x6E:
+        case 0x6F:
+        case 0x70:
+        case 0x71:
+        case 0x72:
+        case 0x73:
+        case 0x74:
+        case 0x75:
+        case 0x76:
+        case 0x77:
+        case 0x78:
+        case 0x79:
+        case 0x7A:
+        case 0x7B:
+        case 0x7C:
+        case 0x7D:
+        case 0x7E:
+        case 0x7F:
+        {
+            if (low >> 4 == 0x04) // 0x40
+            {
+                if (low & 0x0F < 0x08)
+                {
+                    // BIT 0, src
+                    f_register_set_z(~(bit1 & *src));
+                    f_register_set_n(false);
+                    f_register_set_h(true);
+                }
+                else
+                {
+                    // BIT 1, src
+                    f_register_set_z(~(bit2 & *src));
+                    f_register_set_n(false);
+                    f_register_set_h(true);
+                }
+            }
+            else if (low >> 4 == 0x05) // 0x50
+            {
+                if (low & 0x0F < 0x08)
+                {
+                    // BIT 2, src
+                    f_register_set_z(~(bit3 & *src));
+                    f_register_set_n(false);
+                    f_register_set_h(true);
+
+                }
+                else
+                {
+                    // BIT 3, src
+                    f_register_set_z(~(bit4 & *src));
+                    f_register_set_n(false);
+                    f_register_set_h(true);
+                }
+
+            }
+            else if (low >> 4 == 0x05) // 0x60
+            {
+                if (low & 0x0F < 0x08)
+                {
+                    // BIT 4, src
+                    f_register_set_z(~(bit5 & *src));
+                    f_register_set_n(false);
+                    f_register_set_h(true);
+
+                }
+                else
+                {
+                    // BIT 5, src
+                    f_register_set_z(~(bit6 & *src));
+                    f_register_set_n(false);
+                    f_register_set_h(true);
+                }
+
+            }
+            else // 0x70
+            {
+                if (low & 0x0F < 0x08)
+                {
+                    // BIT 6, src
+                    f_register_set_z(~(bit7 & *src));
+                    f_register_set_n(false);
+                    f_register_set_h(true);
+
+                }
+                else
+                {
+                    // BIT 7, src
+                    f_register_set_z(~(bit8 & *src));
+                    f_register_set_n(false);
+                    f_register_set_h(true);
+                }
+
+            }
+
+            break;
+        }
+        // RES
+        case 0x80:
+        case 0x81:
+        case 0x82:
+        case 0x83:
+        case 0x84:
+        case 0x85:
+        case 0x86:
+        case 0x87:
+        case 0x88:
+        case 0x89:
+        case 0x8A:
+        case 0x8B:
+        case 0x8C:
+        case 0x8D:
+        case 0x8E:
+        case 0x8F:
+        case 0x90:
+        case 0x91:
+        case 0x92:
+        case 0x93:
+        case 0x94:
+        case 0x95:
+        case 0x96:
+        case 0x97:
+        case 0x98:
+        case 0x99:
+        case 0x9A:
+        case 0x9B:
+        case 0x9C:
+        case 0x9D:
+        case 0x9E:
+        case 0x9F:
+        case 0xA0:
+        case 0xA1:
+        case 0xA2:
+        case 0xA3:
+        case 0xA4:
+        case 0xA5:
+        case 0xA6:
+        case 0xA7:
+        case 0xA8:
+        case 0xA9:
+        case 0xAA:
+        case 0xAB:
+        case 0xAC:
+        case 0xAD:
+        case 0xAE:
+        case 0xAF:
+        case 0xB0:
+        case 0xB1:
+        case 0xB2:
+        case 0xB3:
+        case 0xB4:
+        case 0xB5:
+        case 0xB6:
+        case 0xB7:
+        case 0xB8:
+        case 0xB9:
+        case 0xBA:
+        case 0xBB:
+        case 0xBC:
+        case 0xBD:
+        case 0xBE:
+        case 0xBF:
+        {
+            if (low >> 4 == 0x08) // 0x80
+            {
+                if (low & 0x0F < 0x08)
+                {
+                    // RES 0, src
+                    *src &= ~bit1;
+                }
+                else
+                {
+                    // RES 1, src
+                    *src &= ~bit2;
+                }
+            }
+            else if (low >> 4 == 0x09) // 0x90
+            {
+                if (low & 0x0F < 0x08)
+                {
+                    // RES 2, src
+                    *src &= ~bit3;
+                }
+                else
+                {
+                    // RES 3, src
+                    *src &= ~bit4;
+                }
+            }
+            else if (low >> 4 == 0x0A) // 0xA0
+            {
+                if (low & 0x0F < 0x08)
+                {
+                    // RES 4, src
+                    *src &= ~bit5;
+                }
+                else
+                {
+                    // RES 5, src
+                    *src &= ~bit6;
+                }
+            }
+            else // 0xB0
+            {
+                if (low & 0x0F < 0x08)
+                {
+                    // RES 6, src
+                    *src &= ~bit7;
+                }
+                else
+                {
+                    // RES 7, src
+                    *src &= ~bit8;
+                }
+            }
+
+            break;
+        }
+
+        // SET
+        case 0xC0:
+        case 0xC1:
+        case 0xC2:
+        case 0xC3:
+        case 0xC4:
+        case 0xC5:
+        case 0xC6:
+        case 0xC7:
+        case 0xC8:
+        case 0xC9:
+        case 0xCA:
+        case 0xCB:
+        case 0xCC:
+        case 0xCD:
+        case 0xCE:
+        case 0xCF:
+        case 0xD0:
+        case 0xD1:
+        case 0xD2:
+        case 0xD3:
+        case 0xD4:
+        case 0xD5:
+        case 0xD6:
+        case 0xD7:
+        case 0xD8:
+        case 0xD9:
+        case 0xDA:
+        case 0xDB:
+        case 0xDC:
+        case 0xDD:
+        case 0xDE:
+        case 0xDF:
+        case 0xE0:
+        case 0xE1:
+        case 0xE2:
+        case 0xE3:
+        case 0xE4:
+        case 0xE5:
+        case 0xE6:
+        case 0xE7:
+        case 0xE8:
+        case 0xE9:
+        case 0xEA:
+        case 0xEB:
+        case 0xEC:
+        case 0xED:
+        case 0xEE:
+        case 0xEF:
+        case 0xF0:
+        case 0xF1:
+        case 0xF2:
+        case 0xF3:
+        case 0xF4:
+        case 0xF5:
+        case 0xF6:
+        case 0xF7:
+        case 0xF8:
+        case 0xF9:
+        case 0xFA:
+        case 0xFB:
+        case 0xFC:
+        case 0xFD:
+        case 0xFE:
+        case 0xFF:
+        {
+            if (low >> 4 == 0x0C) // 0xC0
+            {
+                if (low & 0x0F < 0x08)
+                {
+                    // SET 0, src
+                    *src |= bit1;
+                }
+                else
+                {
+                    // SET 1, src
+                    *src |= bit2;
+                }
+            }
+            else if (low >> 4 == 0x0D) // 0xD0
+            {
+                if (low & 0x0F < 0x08)
+                {
+                    // SET 2, src
+                    *src |= bit3;
+                }
+                else
+                {
+                    // SET 3, src
+                    *src |= bit4;
+                }
+            }
+            else if (low >> 4 == 0x0E) // 0xE0
+            {
+                if (low & 0x0F < 0x08)
+                {
+                    // SET 4, src
+                    *src |= bit5;
+                }
+                else
+                {
+                    // SET 5, src
+                    *src |= bit6;
+                }
+            }
+            else // 0xF0
+            {
+                if (low & 0x0F < 0x08)
+                {
+                    // SET 6, src
+                    *src |= bit7;
+                }
+                else
+                {
+                    // SET 7, src
+                    *src |= bit8;
+                }
+            }
+
+            break;
+        }
+    }
 }
